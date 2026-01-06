371def6fe66389df226da177c25f6ee69a03c722
Peter Bjorklund <piot@hotmail.com>
2026-01-07 00:23:49 +0100

    fix: start work on linux install

--- Formula/lily.rb
+++ Formula/lily.rb
@@ -13,21 +13,27 @@ class Lily < Formula
 
     on_intel do
       url "https://github.com/lily2d/lily2d/releases/download/v#{version}/lily-#{version}-macos-x86_64.tar.gz"
-      sha256 "sha256:1c644af06e8b47d73b4a5a9de1d000bb3541d142b1947fa98c0b7a1eddc6914e"
+      sha256 "1c644af06e8b47d73b4a5a9de1d000bb3541d142b1947fa98c0b7a1eddc6914e"
     end
   end
 
-  def install
-    os_arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
-
-    # version is not part of the executable inside the .tar.gz
-    binary_name = "lily-macos-#{os_arch}"
-
-    bin.install binary_name => "lily"
+  on_linux do
+    on_intel do
+      url "https://github.com/lily2d/lily2d/releases/download/v#{version}/lily-#{version}-linux-x86_64.tar.gz"
+      sha256 ""
+    end
   end
 
-  test do
+def install
+    os = OS.mac? ? "macos" : "linux"
+
+    arch = Hardware::CPU.arm? ? (OS.mac? ? "arm64" : "aarch64") : "x86_64"
+
+    bin.install "lily-#{os}-#{arch}" => "lily"
+  end
+
+  #test do
     # verify the version of the binary
-    assert_match "lily v#{version}", shell_output("#{bin}/lily help")
-  end
+  #  assert_match "lily v#{version}", shell_output("#{bin}/lily help")
+  #end
 end
